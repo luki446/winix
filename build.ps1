64d47b82619dc2db374d48cee141cc2cb41691f7
Luki446 <luki446@gmail.com>
2020-06-09 20:40:05 +0200

    Update build system better compile times

--- build.ps1
+++ build.ps1
@@ -15,20 +15,25 @@ Clear-Host
 
 if($Dev) {
     Write-Host "Running development build"
+} else {
+    Write-Host "Running release build"
+    Remove-Item .\bin\*.exe
 }
 
 foreach($file in $sources ) {
     $name = $file.Name.Split(".")[0]
-    $compileError = ""
-    $error.Clear()
-    Invoke-Expression "$CXX $CXXFlags $( $Dev ? $CXXFlagsDev : $CXXFlagsRel ) -o bin\$name.exe $file" -ErrorVariable compileError 2>$null
-    if($error.Count -eq 0) {
-        Write-Host "[C++]: src\$name.cpp => bin\$name.exe"
-    } else {
-        Write-Error "Error in $name.cpp"
-        foreach($errorLine in $compileError) {
-            Write-Host $errorLine
+    if(![System.IO.File]::Exists(".\bin\$name.exe") -or $file.LastWriteTime -gt (Get-Item ".\bin\$name.exe").LastWriteTime) {
+        $compileError = ""
+        $error.Clear()
+        Invoke-Expression "$CXX $CXXFlags $( $Dev ? $CXXFlagsDev : $CXXFlagsRel ) -o bin\$name.exe $file" -ErrorVariable compileError 2>$null
+        if($error.Count -eq 0) {
+            Write-Host "[C++]: src\$name.cpp => bin\$name.exe"
+        } else {
+            Write-Error "Error in $name.cpp"
+            foreach($errorLine in $compileError) {
+                Write-Host $errorLine
+            }
+            break
         }
-        break
     }
 }
